33c05861163335e07fd9a0654723a25acf2295c4
Kevin Strobel <kevinstrobel@outlook.de>
2025-08-10 17:33:12 +0200

    Added canonical link to the homepage (SEO).

--- site/index.php
+++ site/index.php
@@ -25,6 +25,8 @@
 
     <title><?php echo $data->websiteTitle; ?></title>
 
+    <link rel="canonical" href="<?php echo $data->url; ?>" />
+
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
